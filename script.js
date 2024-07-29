caf7f84df2ef9674e5050b64be69aa16824563d7
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-07-29 15:56:43 +0200

    Add DOM and fix bugs

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 console.log("hey")
 const domController = (function(){
-    const button0 = document.querySelector("#b0")
-    const button1 = document.querySelector("#b1")
-    const button2 = document.querySelector("#b2")
-    const button3 = document.querySelector("#b3")
-    const button4 = document.querySelector("#b4")
-    const button5 = document.querySelector("#b5")
-    const button6 = document.querySelector("#b6")
-    const button7 = document.querySelector("#b7")
-    const button8 = document.querySelector("#b8")
+  const newGame = document.querySelector(".new-game")
+  const text = document.querySelector(".text");
+  newGame.addEventListener("click",()=> game.newGame())
+    
     const buttons = document.querySelectorAll(".button")
     const updateBoard = function(){
             buttons.forEach(function(current){
@@ -17,8 +12,12 @@ const domController = (function(){
     }
     
     
-    return{updateBoard,buttons}
+    return{updateBoard,buttons,text}
     })()
+
+
+
+
 const gameboard = (function(){
     let board = ["","","","","","","","",""];
     const addThing = function(player,position){
@@ -34,6 +33,9 @@ const gameboard = (function(){
 
     
 })()
+
+
+
 const createPlayer = function(name,type){
     let score = 0
     const addScore = ()=> score++
@@ -42,39 +44,77 @@ const createPlayer = function(name,type){
 }
 
 
+
+
+
 const game = (function(){
     let player1,player2;
-    const newGame = function(){
-        let type1 = prompt("Player 1 name:")
-        let type2 = prompt("Player 2 name:")
-        player1 =  createPlayer(type1,"x")
-        player2 =  createPlayer(type2,"o")
-        
-    }
-    const playGame = function(){
+   const newGame = function(){
+        let name1 = prompt("Player 1 name:")
+        let name2 = prompt("Player 2 name:")
+        player1 =  createPlayer(name1,"x")
+        player2 =  createPlayer(name2,"o")
         gameboard.cleanUp();
-        let counter = 2;
-        for(let i = 9;i > 0;i--){
-      if  ( game.checkWin() == true){
-        player1.addScore()
-            console.log(`player ${player1.name} has won! Score: ${player1.getScore()}`)
-            counter = 2
-            break;
-      }
-      else if (counter%2 == 0){
-        domController.buttons.forEach(function(current){
-            current.addEventListener("click",function(){
-            gameboard.addThing(player1.type,current.getAttribute("number"))
-        })})
-       
-       counter++
-       domController.updateBoard()
-      }
-      else {
-        gameboard.addThing(player2.type,prompt("where to put thing player 2"))
-        counter++
         domController.updateBoard()
-      }}  }
+        counter=2;
+        console.log(counter)
+        game.playGame();
+  
+
+        
+    } 
+    
+    const playGame = function(){
+      let counter = 2;
+      console.log(counter)
+        domController.buttons.forEach(element => {
+          element.addEventListener("click",function(){
+            console.log(counter)
+            if (counter % 2 == 0){
+              
+              domController.text.textContent = "P2's turn"
+              gameboard.addThing(player1.type,element.getAttribute("number"))
+              
+              domController.updateBoard();
+              counter++
+              if (game.checkWin()==true){
+                console.log("p1 wins ez gg");
+                counter=2
+                domController.text.textContent = "p1 wins game is over"
+                domController.buttons.forEach(element =>{
+                  element.removeEventListener("click",function(){})
+                  
+                })
+                player1 = {}
+                  player2 = {}
+              }
+              
+            }
+            else if (counter%2 != 0){
+              gameboard.addThing(player2.type,element.getAttribute("number"))
+              domController.text.textContent = "P1 's turn"
+              domController.updateBoard();
+              counter++;
+              if(game.checkWin() == true){
+                console.log("p2wins gg ez")
+                counter=2
+                domController.text.textContent = "P2 won ez"
+                domController.buttons.forEach(element =>{
+                  element.removeEventListener("click",function(){})
+
+                })
+                player1 = {}
+                  player2 = {}
+              }
+            }
+            
+          })
+          
+        })};
+       
+     //////////////////////////////////////////////////////////////////////////////  
+
+      ////////////////////////////////////////////////////////////////////////////////
     const checkWin = function(){
         let zero = gameboard.getBoard()[0]
         let one = gameboard.getBoard()[1]
@@ -86,27 +126,28 @@ const game = (function(){
         let seven = gameboard.getBoard()[7]
         let eight = gameboard.getBoard()[8]    
         if (zero === one && one === two && zero !== ""){
-          if (player1.type === zero){     
-            return true
-          }
+          return true
         }
         else if (three === four && four === five && five !== ""){
+          return true
         }
         else if (six === seven && seven === eight && eight !== ""){  
+          return true
         }
         else if (zero === three && three === six && six !== ""){ 
+          return true
         }
         else if (one === four && four === seven && seven !== ""){
-            
+          return true
         }
         else if (two === five && five === eight && eight !== ""){
-            
+          return true 
         }
         else if (zero === four && four === eight && eight !== ""){
-            
+          return true
         }
         else if (two === four && four === six && six !== ""){
-            
+          return true
         }
        else{
         console.log("try again")
@@ -114,36 +155,16 @@ const game = (function(){
     }
 
     
-    return {checkWin,newGame,playGame}
+    return {checkWin,playGame,newGame}
 })()
 
 
 domController.updateBoard()
-game.newGame()
-game.playGame()
+
 
 /* 
-
-gameboard as an array, inside of gameboard object.
-=> IIFE
-players as objects
-=> not IIFE
-
-game as object
-=> IIFE
-
-
-WHAT> DOES PLAYER NEED
-
-WHAT DOES GAME NEED
-=> checkfor wins => check for combinations => get the value => get the relevat player => call the player stuff
- => call player functions
-
-starting a new turn (switch to other player)  => game object
-adding new symbol where needed => cant add where its already => gameboard object
-checking for a win => or a draw => gtame object
-
-cleaning the board
+when you win a poput text appears sating who won, and the score of the player (score has tyo get update BTW )
+it says whos tunr it is everyturn with text
 
 
 */
